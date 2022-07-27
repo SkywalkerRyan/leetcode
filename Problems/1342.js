302bcc06b98d68ffb18576d2fff9890a71e77102
Ryan Mansur <ryan.mansur@elmelo.com>
2022-07-28 04:44:55 +0600

    Problem 1342.

---
+++ Problems/1342.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function( num ) 
+{
+    count = 0;
+    while( num > 0 )
+    {
+        if( num % 2 === 1 ) num = num - 1
+        else num = num / 2;
+
+        count += 1;
+    }
+
+    console.log( count );
+    return count;
+};
