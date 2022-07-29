8ed53b845aae7b35c6666c7de7d0bfa103d83606
Ryan Mansur <ryan.mansur@elmelo.com>
2022-07-29 08:14:18 +0600

    Problem 0014 - 2.

--- Problems/0014.js
+++ Problems/0014.js
@@ -2,18 +2,21 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function( strs ) 
+var longestCommonPrefix1 = function( strs ) 
 {
-	let val = '';
+	let val = "";
+	for ( let i = 0; i < strs[0].length; i++ ) {
+		if (!strs.every((str) => str[i] === strs[0][i])) { break; }
+		val += strs[0][i];
+	}
+	return val;
+};
+var longestCommonPrefix2 = function( strs ) 
+{
+	let val = strs[0];
 
-	for( let i = 0; i < strs.length; i++ )
-	{
-		if ( i == 0 )
-		{
-			val = strs[0];
-			continue;
-		}
-		
+	for( let i = 1; i < strs.length; i++ )
+	{		
 		let length = val.length > strs[i].length ? strs[i].length : val.length;
 
 		let temp = '';
@@ -34,6 +37,6 @@ var longestCommonPrefix = function( strs )
 	return val;
 };
 
-console.log( longestCommonPrefix( ["ab","a"] ) );
-// console.log( longestCommonPrefix( ["flower","flow","flight"] ) );
-// console.log( longestCommonPrefix( ["dog","racecar","car"] ) );
+// console.log( longestCommonPrefix1( ["ab","a"] ) );
+console.log( longestCommonPrefix1( ["flower","flow","flight"] ) );
+// console.log( longestCommonPrefix1( ["dog","racecar","car"] ) );
